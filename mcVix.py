b83629a39cb18a549b9b8a863157b43e7717a85e
Felipe Belsholff <felipebelsholffpina@hotmail.com>
2019-05-28 21:05:41 -0300

    fixed bug: getting medical slots now is OK.

--- mcVix.py
+++ mcVix.py
@@ -44,18 +44,22 @@ def busca_especialidades(browser, URL):
 ######### SELECIONANDO ESPECIALIDADE E CONSTRUÍNDO LISTA DE VAGAS #########
 
 def coleta_vagas(browser, lista_especialidades, esp):
-    browser.open(lista_especialidades[esp]['link'])
-    lista_vagas = []
-    for item in browser.find_all(attrs={'class': 'col-md-3'}):
-        nome_medico = item.find_all('a', limit=2)[1].text.encode('utf-8')
-        # print nome_medico
-
-        if (item.find(attrs={'class': 'alert alert-warning'}) != 'None' ):
-            lista_vagas.append({'nome': nome_medico, 'vagas': 0})
-            # print lista_vagas
-        elif (item.find(attrs={'class': 'label label-success'}) != 'None' ):
-            nVagas = int(item.text[0])
-            lista_vagas.append({'nome': nome_medico, 'vagas': nVagas})
+    browser.open(lista_especialidades[esp]['link']) #lança exceção se o site não abrir
+    if browser.url == lista_especialidades[esp]['link']:
+        lista_vagas = []
+        for item in browser.find_all('h4'):
+            nome_medico = item.contents[1].text.encode('utf-8')
+            # print nome_medico
+            tag = item.next_sibling.next_sibling
+            if tag.name == 'div':
+                lista_vagas.append({'nome': nome_medico, 'vagas': 0})
+            elif tag.name == 'span':
+                lista_vagas.append({'nome': nome_medico, 'vagas': int(tag.text.split()[0])})
+            else:
+                error_log = open('error.log', 'a')
+                error_log.writelines('[ '+ strftime("%Y-%m-%d | %H:%M:%S -0300", localtime()) + " ] ¬¬ Erro: possivelmente alguma estrutura do site mudou.\n")
+                error_log.close()
+                return lista_vagas
 
     return lista_vagas
 
@@ -84,17 +88,17 @@ def main():
         browser = RoboBrowser(parser='lxml', history=True, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0.4) Gecko/20100101 Firefox/66.0')
 
         browser = login_mcv(browser, URI_login, nCPF, nDNASC)
-        if browser.url != 'https://minhaconsulta.vitoria.es.gov.br/Rede.Bem.Estar/Agendamento/Aviso-Horario':
+        if browser.url == 'https://minhaconsulta.vitoria.es.gov.br/Rede.Bem.Estar/Agendamento/Lista-Consultas':
             lista_especialidades = busca_especialidades(browser, URL)
-            listao = coleta_vagas(browser, lista_especialidades, entrada_especialidade)
+            if len(lista_especialidades) != 0:
+                listao = coleta_vagas(browser, lista_especialidades, entrada_especialidade)
+                ######### SE O NUM DE VAGAS FOR MAIOR QUE 0, ENVIA PRO TELEGRAM #########
+                num_vagas = 0
+                for item in listao:
+                    num_vagas += item['vagas']
 
-            ######### SE O NUM DE VAGAS FOR MAIOR QUE 0, ENVIA PRO TELEGRAM #########
-            num_vagas = 0
-            for item in listao:
-                num_vagas += item['vagas']
-
-            if num_vagas != 0:
-                envia_telegram(browser, listao, TOKEN_BOT_TELEGRAM, CHAT_ID)
+                if num_vagas != 0:
+                    envia_telegram(browser, listao, TOKEN_BOT_TELEGRAM, CHAT_ID)
 
         time.sleep(INT_ATUALIZACAO) #executa a cada 40 segundos
 
